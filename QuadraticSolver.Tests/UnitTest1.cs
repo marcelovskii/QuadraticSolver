0844f219378accffdae6a87a40a025220bc3f080
Marcel <marcel.styla98@gmail.com>
2025-01-27 20:40:18 +0100

    Dodano projekt QuadraticSolver z testami jednostkowymi

---
+++ QuadraticSolver.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Quadratic_Solver;
+using System;
+using Xunit;
+
+public class QuadraticSolverTests
+{
+    [Theory]
+    [InlineData(1, -3, 2, 2, 2.0, 1.0)] // Dwa pierwiastki: x1=2, x2=1
+    [InlineData(1, 2, 1, 1, -1.0, null)] // Jeden pierwiastek: x1=-1
+    [InlineData(1, 0, 1, 0, null, null)] // Brak pierwiastków rzeczywistych
+    public void TestSolve(double a, double b, double c, int expectedRoots, double? expectedX1, double? expectedX2)
+    {
+        var result = QuadraticSolver.Solve(a, b, c);
+        Assert.Equal(expectedRoots, result.Item1);
+        Assert.Equal(expectedX1, result.Item2);
+        Assert.Equal(expectedX2, result.Item3);
+    }
+
+    [Fact]
+    public void TestNonQuadraticEquation()
+    {
+        Assert.Throws<ArgumentException>(() => QuadraticSolver.Solve(0, 2, 1));
+    }
+}
---
+++ Quadratic_Solver/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Quadratic_Solver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+        }
+    }
+
+    public class QuadraticSolver
+    {
+        public static (int, double?, double?) Solve(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                throw new ArgumentException("To nie jest równanie kwadratowe!");
+            }
+
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0)
+            {
+                return (0, null, null);
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                return (1, x, null);
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                return (2, x1, x2);
+            }
+        }
+    }
+}
